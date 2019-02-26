f1783ce4ae71014083bde8b6c7759c2467032b6d
fstltna <fstltna@yahoo.com>
2019-02-26 04:19:53 -0800

    First checkin

---
+++ README.md
@@ -0,0 +1,15 @@
+# Teeworlds backup script (1.0)
+Creates a backup of your Teeworlds folder
+
+Official support sites: [Official Github Repo](https://github.com/fstltna/TeeworldsBackup) - [Official Forum](https://gameplayer.club/phpBB3/viewforum.php?f=8)  - [Official Download Area](https://gameplayer.club/index.php/downloads/category/13-teeworlds)
+
+---
+
+1. Edit the settings at the top of teeworldsbackup.pl if needed
+2. create a cron job like this:
+
+        1 1 * * * /root/TeeworldsBackup/teeworldsbackup.pl
+
+3. This will back up your Teeworlds installation at 1:01am each day, and keep the last 5 backups.
+
+If you need more help visit https://gameplayer.club/phpBB3/viewforum.php?f=8
---
+++ teeworldsbackup.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+# Set these for your situation
+my $TEEWORLDSDIR = "/root/twserver";
+my $BACKUPDIR = "/root/backups";
+my $TARCMD = "/bin/tar czf";
+
+#-------------------
+# No changes below here...
+#-------------------
+my $VERSION = "1.0";
+
+print "Teeworlds Backup version $VERSION\n";
+print "========================\n";
+if (! -d $BACKUPDIR)
+{
+	print "Backup dir $BACKUPDIR not found, creating...\n";
+	system("mkdir -p $BACKUPDIR");
+}
+print "Moving existing backups: ";
+
+if (-f "$BACKUPDIR/teeworldsnbackup-5.tgz")
+{
+	unlink("$BACKUPDIR/teeworldsnbackup-5.tgz")  or warn "Could not unlink $BACKUPDIR/teeworldsnbackup-5.tgz: $!";
+}
+if (-f "$BACKUPDIR/teeworldsnbackup-4.tgz")
+{
+	rename("$BACKUPDIR/teeworldsnbackup-4.tgz", "$BACKUPDIR/teeworldsnbackup-5.tgz");
+}
+if (-f "$BACKUPDIR/teeworldsnbackup-3.tgz")
+{
+	rename("$BACKUPDIR/teeworldsnbackup-3.tgz", "$BACKUPDIR/teeworldsnbackup-4.tgz");
+}
+if (-f "$BACKUPDIR/teeworldsnbackup-2.tgz")
+{
+	rename("$BACKUPDIR/teeworldsnbackup-2.tgz", "$BACKUPDIR/teeworldsnbackup-3.tgz");
+}
+if (-f "$BACKUPDIR/teeworldsnbackup-1.tgz")
+{
+	rename("$BACKUPDIR/teeworldsnbackup-1.tgz", "$BACKUPDIR/teeworldsnbackup-2.tgz");
+}
+print "Done\nCreating New Backup: ";
+system("$TARCMD $BACKUPDIR/teeworldsnbackup-1.tgz $TEEWORLDSDIR");
+print("Done!\n");
+exit 0;
